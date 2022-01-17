2119111db407a312ac9d3c69b3b07441be1cb12a
wuxin <hello_wuxin@qq.com>
2022-01-17 19:05:49 +0800

    编译react

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
+  entry: './src/main.jsx',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
+  resolve: {
+    extensions: ['.jsx', '...']
+  },
   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
+    new HtmlWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }],
+              ["@babel/preset-react"],
+            ]
+          }
+        }
+      }
+    ]
+  },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },

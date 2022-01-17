3a83463bdc22a9fad27e89ddadd83f91ceafbaf5
wuxin <hello_wuxin@qq.com>
2022-01-17 16:48:29 +0800

    Revert "加载css"
    This reverts commit c9ac7d21c821d95f02f0781a94ed3e6ae12f53f2.

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'], // 逆序执行
-      },
-    ],
   },
 };
